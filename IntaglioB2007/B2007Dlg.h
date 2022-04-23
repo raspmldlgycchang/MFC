a4ec605334eb5f22a6cbda055d6520150e7a9d45
raspmldlgycchang <raspmldlgycchang8753@naver.com>
2022-04-23 16:30:49 +0900

    rename at local for local folder_not need rm command

---
+++ IntaglioB2007/B2007Dlg.h
@@ -0,0 +1,38 @@
+﻿
+// B2007Dlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CB2007Dlg 대화 상자
+class CB2007Dlg : public CDialogEx
+{
+private:
+	CPen m_grid_pen;
+	char m_step=0;
+	char m_dol[19][19];
+public:
+	CB2007Dlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_B2007_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+};
